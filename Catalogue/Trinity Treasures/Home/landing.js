ab512d1f631e53b9bfda31cd4ecad01e1a65629f
alabeni temitayo <alabenitemitayo@gmail.com>
2024-02-05 10:48:33 +0100

    update

---
+++ Catalogue/Trinity Treasures/Home/landing.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    speed: 400,
+    preventClicks: true,
+    noSwiping: true,
+    freeMode: false,
+    navigation: {
+        nextEl: ".next",
+        prevEl: ".prev",
+    },
+    breakpoints: {
+        550: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        950: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+    },
+});
